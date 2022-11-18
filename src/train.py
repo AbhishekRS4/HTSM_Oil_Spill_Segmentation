55e3e2e4c5fe62943980c904b4878409901c56cd
AbhishekRS4 <abhishek.r.satyanarayana.4@gmail.com>
2022-11-18 23:57:12 +0100

    added initial implementation of PolynomialLR scheduler

--- src/train.py
+++ src/train.py
@@ -16,6 +16,18 @@ from seg_models import *
 from logger_utils import CSVWriter
 from dataset import get_dataloaders_for_training
 
+from torch.optim.lr_scheduler import _LRScheduler
+
+class PolynomialLR(_LRScheduler):
+    def __init__(self, optimizer, max_iters, power=0.9, last_epoch=-1, min_lr=1e-6):
+        self.power = power
+        self.max_iters = max_iters  # avoid zero lr
+        self.min_lr = min_lr
+        super(PolynomialLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        return [ max( base_lr * ( 1 - self.last_epoch/self.max_iters )**self.power, self.min_lr)
+                for base_lr in self.base_lrs]
 
 def validation_loop(dataset_loader, model, ce_loss, device):
     model.eval()
@@ -107,7 +119,7 @@ def batch_train(FLAGS):
             momentum=0.9,
             weight_decay=FLAGS.weight_decay
         )
-        lr_scheduler = torch.optim.lr_scheduler.PolynomialLR(
+        lr_scheduler = PolynomialLR(
             optimizer, FLAGS.num_epochs+1, power=0.9,
         )
     elif FLAGS.which_optimizer == "adamw":
