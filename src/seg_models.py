468706c6a482dc873c46c28dc6cce4ab2af52202
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2022-12-11 19:55:20 +0100

    added deeplab_v3 segmentation models using efficientnet models

--- src/seg_models.py
+++ src/seg_models.py
@@ -3,8 +3,8 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 
-from decoder_models import DeepLabV3Plus
-from encoder_models import resnet18, resnet34, resnet50, resnet101
+from decoder_models import DeepLabV3Plus, DeepLabV3
+from encoder_models import resnet18, resnet34, resnet50, resnet101, efficientnet_v2_s, efficientnet_v2_m, efficientnet_v2_l
 
 class ResNet18DeepLabV3Plus(nn.Module):
     def __init__(self, num_classes, pretrained=True):
@@ -59,5 +59,56 @@ class ResNet101DeepLabV3Plus(nn.Module):
         input_shape = x.shape[2:]
         encoded_features = self.encoder(x)
         x = self.segmenter(encoded_features, self.encoder.dict_encoder_features["block_1"])
+        x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
+        return x
+
+class EfficientNetSDeepLabV3(nn.Module):
+    def __init__(self, num_classes, pretrained=True):
+        super().__init__()
+
+        self.encoder = efficientnet_v2_s(pretrained=pretrained)
+        self.segmenter = DeepLabV3(
+            in_channels=self.encoder.num_channels_final_block,
+            num_classes=num_classes,
+        )
+
+    def forward(self, x):
+        input_shape = x.shape[2:]
+        encoded_features = self.encoder(x)
+        x = self.segmenter(encoded_features)
+        x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
+        return x
+
+class EfficientNetMDeepLabV3(nn.Module):
+    def __init__(self, num_classes, pretrained=True):
+        super().__init__()
+
+        self.encoder = efficientnet_v2_m(pretrained=pretrained)
+        self.segmenter = DeepLabV3(
+            in_channels=self.encoder.num_channels_final_block,
+            num_classes=num_classes,
+        )
+
+    def forward(self, x):
+        input_shape = x.shape[2:]
+        encoded_features = self.encoder(x)
+        x = self.segmenter(encoded_features)
+        x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
+        return x
+
+class EfficientNetLDeepLabV3(nn.Module):
+    def __init__(self, num_classes, pretrained=True):
+        super().__init__()
+
+        self.encoder = efficientnet_v2_l(pretrained=pretrained)
+        self.segmenter = DeepLabV3(
+            in_channels=self.encoder.num_channels_final_block,
+            num_classes=num_classes,
+        )
+
+    def forward(self, x):
+        input_shape = x.shape[2:]
+        encoded_features = self.encoder(x)
+        x = self.segmenter(encoded_features)
         x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
         return x
