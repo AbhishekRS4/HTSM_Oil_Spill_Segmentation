ceedb86dbc36b6893920869bc6a237ac51802f94
abhishekrs4 <abhishek.r.satyanarayana.4@gmail.com>
2023-01-01 22:27:08 +0100

    minor changes

--- src/train.py
+++ src/train.py
@@ -19,14 +19,14 @@ from logger_utils import CSVWriter, write_dict_to_json
 from torch.optim.lr_scheduler import _LRScheduler
 
 class PolynomialLR(_LRScheduler):
-    def __init__(self, optimizer, max_iters, power=0.9, last_epoch=-1, min_lr=1e-6):
+    def __init__(self, optimizer, max_epochs, power=0.9, last_epoch=-1, min_lr=1e-6):
         self.power = power
-        self.max_iters = max_iters  # avoid zero lr
-        self.min_lr = min_lr
+        self.max_epochs = max_epochs
+        self.min_lr = min_lr # avoid zero lr
         super(PolynomialLR, self).__init__(optimizer, last_epoch)
 
     def get_lr(self):
-        return [ max( base_lr * ( 1 - self.last_epoch/self.max_iters )**self.power, self.min_lr)
+        return [ max( base_lr * ( 1 - self.last_epoch/self.max_epochs )**self.power, self.min_lr)
                 for base_lr in self.base_lrs]
 
 def validation_loop(dataset_loader, model, ce_loss, device):
